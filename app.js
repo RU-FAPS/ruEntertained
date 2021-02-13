9cdb3d692a7895ed2f90771054a39c30d2acab8e
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-13 11:02:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 class App {
     constructor() {
+        var canvas = getElementById("CanvasMain");
+        var ctx = canvas.getContext("2d");
         ctx.fillStyle = "#FF0000";
         ctx.fillRect(0, 0, 150, 75);
     }
