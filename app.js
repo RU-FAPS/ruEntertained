8df884f23e2fbc21162baae95b87ffdaeccd711b
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-12 22:37:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,6 @@ class App {
         var ctx = canvas.getContext("2d");
         ctx.fillStyle = "#FF0000";
         ctx.fillRect(0, 0, 150, 75);
-        
-        this.init();
-        this.update();
-        this.animate();
-                
-//         window.addEventListener("resize", this.resize.bind(this));
-    }
-
-    init() {
-
-    }
-    
-    update()    {
-        
-    }
-    
-    resize()    {
-        
-    }
-
-    animate() {
-
     }
 }
 
