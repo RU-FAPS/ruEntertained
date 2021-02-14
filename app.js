3010acc5ee2b80b7d4801c0a44e34a656a261263
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-14 13:29:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ var dim = [canvas.width, canvas.height];
 boxFill(ctx, [0, 0], dim, "#00ff00");
 boxStroke(ctx, [0, 0], dim, "#ff0000", 2);
 
-ctx.fillText("WELCOME ABOARD!", 0.5 * dim[0], 0.5 * dim[1]);
+textPrint("WELCOME ABOARD!", ctx, 36, [dim[0] / 2, dim[1] / 2], "#ffffff", "c");
