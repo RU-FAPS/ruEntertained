64da2b12db88d6fdd6a673486f3b298f12399084
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-22 02:04:12 -0500

    WebXR bug fix attempt

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ var createScene = async function () {
 const scene = createScene(); //Call the createScene function
 
 // Register a render loop to repeatedly render the scene
-engine.runRenderLoop(function () {
-    scene.render();
-});
+//engine.runRenderLoop(function () {
+//    scene.render();
+//});
 
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
