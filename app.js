3b19fa5d286efe7b04ba98c251eed70233dd1aeb
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-22 01:51:21 -0500

    Testing Babylon WebXR

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
 // Add your code here matching the playground format
+//const createScene = function () {
+
+//    const scene = new BABYLON.Scene(engine);
+
+//    BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon");
+
+//    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+//    camera.attachControl(canvas, true);
+//    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+//    return scene;
+//};
+
+var createScene = async function () {
+    var scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(canvas, true);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    sphere.position.y = 1;
+    const env = scene.createDefaultEnvironment();
+    // here we add XR support
+    const xr = await scene.createDefaultXRExperienceAsync({
+        floorMeshes: [env.ground],
+    });
+    return scene;
+};
+
 const scene = createScene(); //Call the createScene function
+
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
     scene.render();
 });
+
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
     engine.resize();
